703d80147405133840e172c0a2806dc480202231
ramanadas <104691675+ramanadas@users.noreply.github.com>
2024-05-05 00:56:46 +0530

    added pop up model
    it is used to show full description on pop up.

---
+++ weekday-assignment/src/components/FullDescriptionModal.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Modal, Typography, Button } from '@mui/material';
+
+const FullDescriptionModal = ({ open, handleClose, fullDescription }) => {
+  return (
+    <Modal open={open} onClose={handleClose}>
+      <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'white', padding: '20px', borderRadius: '10px', width: '80vw', maxWidth: '600px', maxHeight: '80vh', overflowY: 'auto' }}>
+        <Typography variant="body2">{fullDescription}</Typography>
+        <Button onClick={handleClose}>Close</Button>
+      </div>
+    </Modal>
+  );
+};
+
+export default FullDescriptionModal;
